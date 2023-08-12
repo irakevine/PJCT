37ab9cd524312cfa809d22bcc46b4ed1e8064ef4
irakevine <ikevine2016@gmail.com>
2023-08-13 00:23:55 +0200

    adding function

--- foundamentalPart2/final/script.js
+++ foundamentalPart2/final/script.js
@@ -1,6 +1,14 @@
 "use strict";
 
-let hastenqe = false;
-const passP = true;
-if (passP) hastenqe = true;
-if (hastenqe) console.log("i can do that");
+// let hastenqe = false;
+// const passP = true;
+// if (passP) hastenqe = true;
+// if (hastenqe) console.log("i can do that");
+/// function
+function fruits(apples, mango) {
+  console.log(apples, mango);
+  const juice = `juice with ${apples} apples and ${mango} mango`;
+  return juice;
+}
+const appleJuice = fruits(7, 10);
+console.log(appleJuice);
