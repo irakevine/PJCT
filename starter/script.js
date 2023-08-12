cedc76c24910a5c52561008b078e66f9c09c54c3
irakevine <ikevine2016@gmail.com>
2023-08-12 09:49:46 +0200

    challenge on logical operator

--- starter/script.js
+++ starter/script.js
@@ -11,3 +11,30 @@ if (money) {
 } else {
   console.log("you have a job");
 }
+// CHALLENGE #3
+// There are two gymnastics teams: Dolphins and Koalas. They complete against each other 3 times. The winner with the highest average score wins a trophy!
+
+// Your tasks:
+
+// 1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+
+// 2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+
+// "Dolphins win the trophy" if Dolphins win, or
+
+// "Koalas win the trophy" if Koalas win, or
+
+// "Both win the trophy" if their average scores are equal.
+
+// TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
+const Davg = 146.5;
+
+const Kavg = 144.5;
+if (Davg > Kavg) {
+  console.log("Dolphins win the trophy");
+} else {
+  console.log("Koalas win the trophy");
+}
+if (Davg === Kavg) {
+  console.log("Both win the trophy");
+}
