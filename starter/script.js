514565d25a956ae10defdac68d059759b5c7660d
irakevine <ikevine2016@gmail.com>
2023-08-12 23:25:26 +0200

    adding ternary operator

--- starter/script.js
+++ starter/script.js
@@ -64,6 +64,4 @@ const bill = 40;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 const totalValue = bill + tip;
 
-console.log(
-  `the bill ${bill}, the tip was  ${tip},  and the total are ${bill + tip}`
-);
+console.log("all are:", bill, tip, totalValue);
